6e28790746ceb593989aa4bd0da80cbff900d759
刘柏麟 <445133808@qq.com>
2023-02-01 22:01:57 +0800

    feat: type

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-set -e
+
+echo "选择此次发版类型?"
+select type in fix feat;
+do
+    break
+done
+ 
+read  -p "请输入此次发版的内容: " content
+
+
 
 npm run docs:build
 
@@ -12,15 +21,14 @@ git init
 
 git add .
 
-git commit -m '自动部署'
+
+git commit -m "$type: $content"
 
 git remote add origin https://github.com/lovebailin/lovebailin.github.io.git
 
 git branch -m master main
 
 git push -f origin main
-
-
 
 cd -
 
