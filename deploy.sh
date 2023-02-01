d786569e16bad35c53e0e4fd559e1144a705463d
刘柏麟 <445133808@qq.com>
2023-02-01 21:19:29 +0800

    feat: 自动发版

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,21 @@ npm run docs:build
 
 cd docs/.vitepress/dist
 
+echo hello > README.md
+
 git init
 
 git add .
 
 git commit -m '自动部署'
 
-git push -f https://github.com/lovebailin/lovebailin.github.io.git main
+git remote add origin https://github.com/lovebailin/lovebailin.github.io.git
+
+git branch -m master main
+
+git push -f origin main
+
+
 
 cd -
 
